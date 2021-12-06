59cc5ee56d24fb055cac0c085c9e4807f2ca22f1
Smaug123 <patrick+github@patrickstevens.co.uk>
2021-12-06 20:27:17 +0000

    Day 6

--- AdventOfCode2021/Program.fs
+++ AdventOfCode2021/Program.fs
@@ -7,6 +7,9 @@ module Program =
         | "1" ->
             printfn $"{Day1.part1 ()}"
             printfn $"{Day1.part2 ()}"
+        | "6" ->
+            printfn $"{Day6.part1 ()}"
+            printfn $"{Day6.part2 ()}"
         | s -> failwithf $"Unexpected argument: %s{s}"
 
         0
